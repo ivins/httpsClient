9d32b88a1f05ee25d1313da8e9dc6c22396ec03c
silentscribe <seelive.i@gmail.com>
2019-01-14 21:10:08 +0000

    step two written to print concatenated incoming chunks

---
+++ step2.js
@@ -0,0 +1,50 @@
+const https = require('https');
+
+function getAndPrintHTMLChunks () {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk) {
+      console.log(chunk + '\n');
+    });
+
+    response.on('end', function() {
+    console.log('Stream ended');
+  });
+  });
+}
+
+// getAndPrintHTMLChunks();
+
+
+function getAndPrintHTML () {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  let totalResponse = '';
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk) {
+      totalResponse += chunk + '\n';
+    });
+
+    response.on('end', function() {
+    console.log(totalResponse);
+  });
+  });
+}
+
+
+getAndPrintHTML();
+
+
