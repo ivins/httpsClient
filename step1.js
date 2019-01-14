117504c7489d7234674bf84e2d5a4e8d7ed83e6b
silentscribe <seelive.i@gmail.com>
2019-01-14 20:16:51 +0000

    function is printing total data with newlines after each chunk

--- step1.js
+++ step1.js
@@ -6,14 +6,20 @@ function getAndPrintHTMLChunks () {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
-
+  let totalResponse = '';
   https.get(requestOptions, function(response) {
 
     response.setEncoding('utf8');
 
-    response.on('data', function(data) {
-
+    response.on('data', function(chunk) {
+      // totalResponse += chunk + '\n';
+      totalResponse += chunk + '\n';
     });
-  });
 
+    response.on('end', function() {
+    console.log(totalResponse);
+  });
+  });
 }
+
+getAndPrintHTMLChunks();
